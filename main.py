2b9c02a094f9ef4100f7318715b75e9c281ada50
biskecz <biskecz@gmail.com>
2026-08-27 20:36:13 +0200

    add SMA starategy returns

--- main.py
+++ main.py
@@ -18,14 +18,19 @@ signal = (sma_20 > sma_50).astype(int)
 
 daily_return = close.pct_change()
 
-print(signal.tail(20))
-print(daily_return.tail(10))
+strategy_return = signal * daily_return
+
+cumulative_return = (strategy_return + 1).cumprod()
+
+print("последние 20 торговых дней + сигнал: \n", signal.tail(20))
+print("Доходность AAPL за последние 10 торговых дней: \n",daily_return.tail(10))
+print("Последние 5 значений накопленого результа: \n",cumulative_return.tail())
 
 mpl.plot(close, label="AAPL")
 mpl.plot(sma_20, label="SMA 20")
 mpl.plot(sma_50, label="SMA 50")
 
-mpl.title("AAPL Close Price")
+mpl.title("AAPL stock chart, SMA 20 & SMA 50")
 mpl.xlabel("Date")
 mpl.ylabel("Price")
 mpl.legend()
